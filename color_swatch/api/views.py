95f76e7f8e5659534927a92d9dc9d04b0e1f00eb
Samuel <samuelsneyd@gmail.com>
2024-11-30 13:15:36 +1100

    add ColorSwatchBySpaceView api view

--- color_swatch/api/views.py
+++ color_swatch/api/views.py
@@ -28,3 +28,26 @@ class ColorSwatchView(APIView):
 
         swatch = [get_random_color_space().random_color() for _ in range(n)]
         return Response(swatch)
+
+
+class ColorSwatchBySpaceView(APIView):
+    def get(self, request, color_space):
+        space = get_color_space(color_space)
+        if space is None:
+            return Response(
+                {"error": f"invalid color space '{color_space}'"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            n = int(request.query_params.get(N_COLORS_PARAM, N_COLORS_DEFAULT))
+            if not (N_COLORS_MIN < n < N_COLORS_MAX):
+                raise ValueError
+        except ValueError:
+            return Response(
+                {"error": f"'{N_COLORS_PARAM}' must be an int between {N_COLORS_MIN} and {N_COLORS_MAX}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        swatch = [space.random_color() for _ in range(n)]
+        return Response(swatch)
