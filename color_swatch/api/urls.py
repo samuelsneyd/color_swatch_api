dea085989b3d94f3fce0fbd3946a0ef5b4cfe9af
Samuel <samuelsneyd@gmail.com>
2024-11-30 13:14:51 +1100

    add api/colors/ api route

--- color_swatch/api/urls.py
+++ color_swatch/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HealthCheck
+from .views import HealthCheck, ColorSwatchView
 
 urlpatterns = [
-    path('health/', HealthCheck.as_view(), name="health")
+    path('health/', HealthCheck.as_view(), name='health'),
+    path('colors/', ColorSwatchView.as_view(), name='color-swatch'),
 ]
