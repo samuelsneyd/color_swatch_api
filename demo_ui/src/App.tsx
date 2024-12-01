89cb1228425b8d1fa181d4eedb557117cff11496
Samuel <samuelsneyd@gmail.com>
2024-12-01 11:53:54 +1100

    init color swatch app UI

--- demo_ui/src/App.tsx
+++ demo_ui/src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_ENDPOINT = 'http://127.0.0.1:8000';
+
+type ColorSwatch = {
+  type: 'rgb' | 'hsl';
+} & Record<string, any>;
+
+type RGBColorSwatch = {
+  type: 'rbg';
+  red: number;
+  green: number;
+  blue: number;
+}
+
+type HSLColorSwatch = {
+  type: 'hsl';
+  hue: number;
+  saturation: number;
+  lightness: number;
+}
+
+interface ColorSwatchProps extends React.HTMLAttributes<HTMLDivElement> {
+  nColors?: number;
+}
+
+const fetchColors = async ({ nColors }: { nColors?: number } = {}): Promise<ColorSwatch[]> => {
+  const url = new URL('/api/colors', API_ENDPOINT);
+  if (nColors) {
+    url.searchParams.append('n', nColors.toString());
+  }
+  try {
+    return await fetch(url).then(res => res.json());
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
+
+const ColorBox = ({ colorSwatch, ...props }: { colorSwatch: ColorSwatch } & React.HTMLAttributes<HTMLDivElement>) => {
+  let backgroundColor: string | undefined;
+
+  switch (colorSwatch.type) {
+    case 'rgb': {
+      const swatch = colorSwatch as RGBColorSwatch;
+      backgroundColor = '#' + [swatch.red, swatch.green, swatch.blue]
+        .map(n => n.toString(16).padStart(2, '0'))
+        .join('');
+      break;
+    }
+    case 'hsl': {
+      const swatch = colorSwatch as HSLColorSwatch;
+      backgroundColor = `hsl(${swatch.hue} ${swatch.saturation}% ${swatch.lightness}%)`;
+      break;
+    }
+  }
+
+  return (
+    <div className="colorBox" style={{ backgroundColor }} {...props}>
+      <p className="shadow-text">{backgroundColor}</p>
+    </div>
+  );
+};
+
+
+const ColorSwatch = ({ nColors = 5, ...props }: ColorSwatchProps) => {
+  const [colors, setColors] = React.useState<ColorSwatch[]>([]);
+
+  React.useEffect(() => {
+    fetchColors({ nColors }).then(setColors);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container" {...props}>
+        {colors.map((colorSwatch, index) => (
+          <ColorBox key={index} colorSwatch={colorSwatch} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => fetchColors().then(setColors)}>
+        New colors
+      </button>
     </>
-  )
+  );
+};
+
+
+function App() {
+  return (
+    <>
+      <h1>Color Swatch</h1>
+      <ColorSwatch />
+    </>
+  );
 }
 
-export default App
+export default App;
